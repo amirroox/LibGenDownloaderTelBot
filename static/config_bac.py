5959947d04991cb2f627402497df1e60983854dd
Amir Roox <amirroox@yahoo.com>
2024-10-06 14:28:39 +0330

    Initial Config

---
+++ static/config_bac.py
@@ -0,0 +1,27 @@
+# Rename To config.py
+# Amir Roox
+API_ID: int = 111111111
+API_HASH: str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+BOT_TOKEN: str = "1111111:aaaaaaaaaaaaaaaaaaaaaaaa"  # BothFather
+ADMINS_ID = [11111111]
+
+DATABASE_USERNAME: str = ""
+DATABASE_PASSWORD: str = ""
+DATABASE_NAME: str = ""
+
+
+CHANELS = ['']  # Bot Admin (Without @)
+MAIN_ADMIN = 'you_113'  # Author
+MAIN_DOMAIN: str = ''  # Without / End
+NAME_BOT: str = ''
+
+# Limit Config
+MAX_MESSAGES: float = 10  # Count
+TIME_WINDOW: float = 60  # All Time (Second)
+COOLDOWN_TIME: float = 5  # Wait Time (Secend)
+
+# Timeout Delete Massage CopyRight
+TIMEOUT_DELETE: int = 60  # Second
+
+# Limit Users Use Bot
+LIMIT_USERS: int = 15000
